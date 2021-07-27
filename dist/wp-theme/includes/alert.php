4409475a17e8f06cdc405d799e0ceb71ab8d40a1
Bence Agocs <bence@harom.studio>
2021-07-27 11:22:21 +0200

    alert color changed

--- dist/wp-theme/includes/alert.php
+++ dist/wp-theme/includes/alert.php
@@ -1,3 +1,3 @@
 <section class="container mt-3">
-    <p class="alert alert-info" role="alert"><?php the_field('szoveg', 69); ?></p>
+    <p class="alert alert-warning" role="alert"><?php the_field('szoveg', 69); ?></p>
 </section>
